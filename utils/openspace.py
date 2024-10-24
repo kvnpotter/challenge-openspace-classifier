31fded23f0a86b98e774bad0d3be0c11b797ac5c
Kevin Potter <kvn.potter@outlook.com>
2024-10-24 14:57:12 +0200

    Additional edits to visualisation function

--- utils/openspace.py
+++ utils/openspace.py
@@ -1,6 +1,8 @@
 from table import Table
 from random import sample
+from shapely.geometry import Point, Polygon
 import matplotlib.pyplot as plt
+import geopandas as gpd
 
 class Openspace:
     ...
@@ -26,17 +28,73 @@ class Openspace:
 
     def display(self) -> None:
         ...
-        table_coords = []
-        for table in self.tables:
-            number_occupants = table.capacity - table.left_capacity()
-            number_occupants_side = table.capacity- table.capacity // 2
-            table_length = number_occupants_side * 2 + 2
-            for col in range(self.number_of_tables):
-                center_table_x = col * 25 + 12.5
-                for row in range(4):
 
+        # Create empty lists to contain table x and y coordinates, table length and the variable table width
+        table_x_coords = []
+        table_y_coords = []
+        table_lengths = []
+        table_width = 5
+        
+        # Loop over tables, assign column (4 rows of tables per column max), calculate x coordinate center table
+        for i in range(self.number_of_tables):
+            column = round(i/4)
+            center_table_x = column * 25 + 12.5
+            table_x_coords.append(center_table_x)
+        
+        # Create table index, go over all tables in groups of 4 (rows per column), calculate variable table length depending on 
+        # number of occupants, calculate y coordinate center table
+        table_index = 0
+        while table_index + 1 < self.number_of_tables :
+            for i in range(4):
+                number_occupants_side = self.tables[table_index].capacity - self.tables[table_index].capacity // 2
+                table_length = number_occupants_side * 2 + 2
+                table_lengths.append(table_length)
+                center_table_y = (table_length / 2 + 5) + (i * (table_length + 10))
+                table_y_coords.append(center_table_y)
+                table_index += 1
+
+        # Create a list of shapely Point() objects from lists of x and y coordinates
+        tables_points = []
+        for x, y in zip(table_x_coords, table_y_coords):
+            tables_points.append(Point(x,y))
+
+        # Create a list of tables (polygons), each a list of 4 points (corners of each table)
+        tables = []
+        for index, center_point in enumerate(tables_points):
+                point_1 = Point(center_point.x - 2.5, center_point.y + table_lengths[index] / 2)
+                point_2 = Point(center_point.x - 2.5, center_point.y - table_lengths[index] / 2)
+                point_3 = Point(center_point.x + 2.5, center_point.y + table_lengths[index] / 2)
+                point_4 = Point(center_point.x + 2.5, center_point.y - table_lengths[index] / 2)
+                tables.append(Polygon[point_1, point_2, point_3, point_4])
+        
+        # Into GeoDataFrame
+        tables_gdf = gpd.GeoDataFrame(geometry = tables)
+
+        # Create plot and visualise
+
+        f, ax = plt.subplots(1,1)
+        tables_gdf.plot(ax = ax)
+
+                table.append(tables_points[0])
+                tables_points.pop(0)
+                table_index += 1
+            new_table = Polygon(table)
+            tables.append(new_table)
+            table=[]
             
-            center_table_y = (table_length / 2 + 5) + (row_ind * (table_length + 10))
+        while point_counter < 4 :
+            table.append(tables_points[0])
+            tables_points.pop(0)
+            point_counter += 1
+        else:
+            new_table = Polygon(table)
+            tables.append(new_table)
+            table = []
+            point_counter = 0
+
+        number_occupants = self.tables[table_index].capacity - self.tables[table_index].left_capacity()
+            
+            
 
     def store(self, filename: str) -> None:
         ...
