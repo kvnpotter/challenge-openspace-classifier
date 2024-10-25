621b285182304d0be197930e56d3ef16ea7c2202
Kevin Potter <kvn.potter@outlook.com>
2024-10-25 10:27:26 +0200

    str dunder methods added

--- utils/table_kevin.py
+++ utils/table_kevin.py
@@ -11,6 +11,13 @@ class Seat:
         self.occupant = "None"
         self.free = True
 
+    def __str__(self):
+        if self.free:
+            status = "empty"
+        else:
+            status = "occupied"
+        return f"This is a chair.\nIt is currently {status}.\nIt's occupant is {self.occupant}."
+
 class Table:
     def __init__(self, capacity = 4):
         self.capacity = capacity
@@ -37,4 +44,10 @@ class Table:
         taken = 0
         for seat in self.seats:
             taken += not(seat.free)
-        return self.capacity - taken  
+        return self.capacity - taken 
+    
+    def __str__(self):
+        occupants = ""
+        for seat in self.seats:
+            occupants += f"{seat.occupant}/n"
+        return f"This is a table.\nThere are {left_capacity(self)} free seats.\nThe occupants are:\n{occupants}"
